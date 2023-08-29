cbcbb26a5235e0d361828bc5b70ff215f2f834b9
Patriziano <onahpatrick75@gmail.com>
2023-08-29 16:40:27 -0700

    A class that prints in stdout the square with the character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""A class square"""
+
+
+class Square:
+    """
+    A class square that defines a square with private instance size
+    """
+    def __init__(self, size=0):
+        """
+        Initializes an instance of the square class with a given size to 0
+
+        Args:
+            size (int): size of the square
+
+        Raise:
+            TypeError: If size is not integer
+            ValueError: if size is less than 0
+
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Public instance method that returns the current square area
+
+        Returns:
+            current square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        prints in stdout the square with the character #
+
+        prints a empty line if size = 0
+        """
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
+
+    @property
+    def size(self):
+        """
+            Getter - To retrieve the data
+
+            Returns:
+                Retrieved data
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter - To change the data
+
+        Args:
+            Value(int): The value to be set to
+        Returns:
+            The changed data.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
