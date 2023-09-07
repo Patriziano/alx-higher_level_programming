9445464e8b70aa21f2bb1d708be1ac44f412eb3a
Patriziano <onahpatrick75@gmail.com>
2023-09-07 02:35:12 -0700

    fixed size of row

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -22,9 +22,9 @@ def matrix_mul(m_a, m_b):
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
 
-    if not all(isinstance(row, list) for row in m_a):
+    if not any(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
+    if not any(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
 
     if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
@@ -37,9 +37,9 @@ def matrix_mul(m_a, m_b):
     if not all(isinstance(num, (int, float)) for row in m_b for num in row):
         raise TypeError("m_b should contain only integers or floats")
 
-    if all(len(row) != len(m_a[0]) for row in m_a):
+    if any(len(row) != len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
-    if all(len(row) != len(m_b[0]) for row in m_b):
+    if any(len(row) != len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
