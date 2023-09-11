619d8e0f6102f5c6688e44944d5e04b55a0825aa
Patriziano <onahpatrick75@gmail.com>
2023-09-11 15:16:11 -0700

    defining area tht raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Area of the BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    This class can be used as a foundation for defining various geometry classes
+    and methods.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry shape.
+
+        Raises:
+        NotImplementedError: This method must be implemented in derived classes.
+        """
+        raise NotImplementedError("area() is not implemented")
