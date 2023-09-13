bed37cb1843aa2db44c51836951764ac2e3b5efc
Patriziano <onahpatrick75@gmail.com>
2023-09-13 15:27:58 -0700

    creates an object from aJSON file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,15 @@ import json
 
 
 def load_from_json_file(filename):
+    """
+    creates an object from the JSON string
+
+    Args:
+        Filename: name of the file
+
+    Returns:
+        The object of the JSON textfile
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         json_obj = json.load(f)
         return json_obj
