5d460c3b7568fc49c60b2a157d1a0feaa2998904
Patriziano <onahpatrick75@gmail.com>
2023-09-13 18:21:35 -0700

    pycodestyle fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """JSON rep object addition module"""
 from sys import argv
 import os
@@ -7,7 +7,10 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 def add_arguments_to_list(arguments):
+    """a function that adds arguments to list"""
+
     if os.path.exists("add_item.json"):
         existing_list = load_from_json_file("add_item.json")
     else:
@@ -15,8 +18,9 @@ def add_arguments_to_list(arguments):
     existing_list.extend(arguments)
     return existing_list
 
+
 if __name__ == "__main__":
-# Get command-line arguments (excluding the script name)
+    # Get command-line arguments (excluding the script name)
     arguments = argv[1:]
     if arguments:
         updated_list = add_arguments_to_list(arguments)
