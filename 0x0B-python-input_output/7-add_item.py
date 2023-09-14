88544946e660f31ea93234cbe2f0f54df02a6161
Patriziano <onahpatrick75@gmail.com>
2023-09-13 18:17:15 -0700

    a script that adds all arguments to a Python list, and then save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#/usr/bin/python3
+"""JSON rep object addition module"""
+from sys import argv
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+def add_arguments_to_list(arguments):
+    if os.path.exists("add_item.json"):
+        existing_list = load_from_json_file("add_item.json")
+    else:
+        existing_list = []
+    existing_list.extend(arguments)
+    return existing_list
+
+if __name__ == "__main__":
+# Get command-line arguments (excluding the script name)
+    arguments = argv[1:]
+    if arguments:
+        updated_list = add_arguments_to_list(arguments)
+        save_to_json_file(updated_list, "add_item.json")
+#       print("Arguments added to 'add_item.json'.")
+#   else:
+#        print("No arguments provided.")
