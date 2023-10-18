f5fdd079ba5b70b97cbb8a58973a39f6bf9fc90e
Patriziano <onahpatrick75@gmail.com>
2023-10-18 14:36:36 -0700

    listing a column of a table found in a database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- listing all column of a table from a database
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'california');
+WHERE state_id = (SELECT id FROM states WHERE name = 'california'),
+ORDER BY cities.id(ASC)
