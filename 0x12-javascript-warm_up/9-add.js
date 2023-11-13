ce506fe5409770cd8161bbe940d905262159dacf
Patriziano <onahpatrick75.com>
2023-11-13 15:30:41 -0800

    addition of integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+const num1 = parseInt(arg1);
+const num2 = parseInt(arg2);
+
+const result = add(num1, num2);
+console.log(result);
