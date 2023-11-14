9040fb0c696b0951494dab64e4de50077edc5e2a
Patriziano <onahpatrick75.com>
2023-11-14 14:40:51 -0800

    an instance that prints the rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0) {
       // If width or height is <= 0, create an empty object
       return {};
@@ -12,7 +12,7 @@ class Rectangle {
     this.height = h;
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
