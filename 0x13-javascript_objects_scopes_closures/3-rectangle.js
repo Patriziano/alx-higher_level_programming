99f93f4b801937a8af1adbfa90e46f5f199216e7
Patriziano <onahpatrick75.com>
2023-11-14 14:37:20 -0800

    an instance that prints the rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      // If width or height is <= 0, create an empty object
+      return {};
+    }
+
+    // Initialize instance attributes
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
